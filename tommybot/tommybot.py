769d0d60edb69fb02825e8a46eaeeb9f2bbe7092
Thomas Göttsching <support@thomasgoettsching.de>
2024-06-16 22:21:33 +0200

    Testing - Config

--- tommybot/tommybot.py
+++ tommybot/tommybot.py
@@ -1,10 +1,16 @@
-from redbot.core import commands
+from redbot.core import commands, Config
 
 
 class Tommybot(commands.Cog):
     def __init__(self, bot, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.bot = bot
+        self.config = Config.get_conf(self, identifier=1000000000)
+        default_global = {
+            "name": "Red-DiscordBot",
+            "author": "Thomas Göttsching"
+        }
+        self.config.register_global(**default_global)
 
     @commands.command()
     async def hello(self, ctx):
