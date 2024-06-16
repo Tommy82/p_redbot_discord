624d4b797e3cb9c005767eef79fb71f6b2c83880
Thomas Göttsching <support@thomasgoettsching.de>
2024-06-16 22:37:20 +0200

    Testing - Config

--- tommybot/tommybot.py
+++ tommybot/tommybot.py
@@ -1,13 +1,22 @@
 from redbot.core import commands, Config
+from redbot.core.bot import Red
 
 
 class Tommybot(commands.Cog):
-    def __init__(self, bot, *args, **kwargs):
-        self.config = Config.get_conf(self, identifier=1234567890, force_registration=True)
-
-        self.config.register_global(
-            foo=True
+    def __init__(self, bot: Red) -> None:
+        self.bot = bot
+        self.config = Config.get_conf(
+            self,
+            identifier=1000000,
+            force_registration=True,
         )
+        default_guild = {
+            "manager": [],
+            "guessing_time": 60,
+            "voting_time": 60,
+        }
+        self.config.register_guild(**default_guild)
+        self.cache = {}
 
     @commands.command()
     async def hello(self, ctx):
